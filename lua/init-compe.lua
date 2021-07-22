8403b951ad01830e731d1285d1749c784e484b6f
dragondove <jinxiongrong@foxmail.com>
2021-07-22 21:00:33 +0800

    maybe fixed a completion unexpection

--- lua/init-compe.lua
+++ lua/init-compe.lua
@@ -1,30 +1,35 @@
 vim.o.completeopt = "menuone,noselect"
 
 require('compe').setup {
-    enabled = true,
-    autocomplete = true,
-    debug = false,
-    min_length = 1,
-    preselect = "enable",
-    throttle_time = 80,
-    source_timeout = 200,
-    incomplete_delay = 400,
-    max_abbr_width = 100,
-    max_kind_width = 100,
-    max_menu_width = 100,
-    documentation = true,
-    source = {
-        path = true,
-        buffer = true,
-        calc = true,
-        nvim_lsp = true,
-        nvim_lua = true,
-        spell = true,
-        luasnip = true,
-        tags = true,
-        snippets_nvim = true,
-        treesitter = true
-    }
+  enabled = true,
+  autocomplete = true,
+  debug = false,
+  min_length = 1,
+  preselect = "enable",
+  throttle_time = 80,
+  source_timeout = 200,
+  incomplete_delay = 400,
+  max_abbr_width = 100,
+  max_kind_width = 100,
+  max_menu_width = 100,
+  documentation = {
+    border = { '', '' ,'', ' ', '', '', '', ' ' },
+    winhighlight = "NormalFloat:CompeDocumentation,FloatBorder:CompeDocumentationBorder",
+    max_width = 120,
+    min_width = 60,
+    max_height = math.floor(vim.o.lines * 0.3),
+    min_height = 1,
+  };
+  source = {
+      path = true,
+      buffer = true,
+      calc = true,
+      nvim_lsp = true,
+      nvim_lua = true,
+      spell = true,
+      luasnip = true,
+      tags = true
+  }
 }
 
 local luasnip = require('luasnip')
@@ -42,9 +47,6 @@ local check_back_space = function()
     end
 end
 
--- Use (s-)tab to:
---- move to prev/next item in completion menuone
---- jump to prev/next snippet's placeholder
 _G.tab_complete = function()
   if vim.fn.pumvisible() == 1 then
     return t "<C-n>"
