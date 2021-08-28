f6bd8a2c33c203ccfc4c65eac0c5df6554126535
dragove <jinxiongrong@foxmail.com>
2021-08-28 19:18:43 +0800

    simplify auto pairs configuration

--- lua/init-cmp.lua
+++ lua/init-cmp.lua
@@ -46,27 +46,14 @@ cmp.setup {
   }
 }
 
-local remap = vim.api.nvim_set_keymap
-local npairs = require('nvim-autopairs')
-npairs.setup()
+require('nvim-autopairs').setup()
 
--- skip it, if you use another global object
-_G.MUtils= {}
-
-vim.g.completion_confirm_key = ""
-MUtils.completion_confirm=function()
-  if vim.fn.pumvisible() ~= 0  then
-    if vim.fn.complete_info()["selected"] ~= -1 then
-      return vim.fn["compe#confirm"](npairs.esc("<cr>"))
-    else
-      return npairs.esc("<cr>")
-    end
-  else
-    return npairs.autopairs_cr()
-  end
-end
-
-remap('i' , '<CR>','v:lua.MUtils.completion_confirm()', {expr = true , noremap = true})
+-- you need setup cmp first put this after cmp.setup()
+require("nvim-autopairs.completion.cmp").setup({
+  map_cr = true, --  map <CR> on insert mode
+  map_complete = true, -- it will auto insert `(` after select function or method item
+  auto_select = true -- automatically select the first item
+})
 
 -- init surround
 require('surround').setup {}
