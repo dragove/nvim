794b9c58c21b986bc5205e2e19a5042e49e82637
jingchun <jingchun@tsign.cn>
2021-09-30 16:50:33 +0800

    Fix no indent while isnerting tab

--- lua/init-misc.lua
+++ lua/init-misc.lua
@@ -1,19 +1,20 @@
 vim.o.hidden        = true      -- Keep multiple buffers open
 vim.o.splitright    = true      -- split to the right in vsplit
 vim.o.splitbelow    = true      -- Split to the bottom in split
-vim.o.updatetime    = 250 -- Faster completion
-vim.o.timeoutlen    = 500 -- timeout for which key
-vim.o.mouse         = "a" -- Enable mouse for any mode
-vim.o.errorbells    = false -- Disable sounds for errors
+vim.o.updatetime    = 250       -- Faster completion
+vim.o.timeoutlen    = 500       -- timeout for which key
+vim.o.mouse         = "a"       -- Enable mouse for any mode
+vim.o.errorbells    = false     -- Disable sounds for errors
 vim.o.writebackup   = false
 vim.o.termguicolors = true
 
 vim.bo.smartindent  = true
+vim.bo.expandtab    = true
 vim.bo.shiftwidth   = 2
 
-vim.wo.number       = true -- Show line number
-vim.wo.cursorline   = true -- Highlight current line
-vim.wo.wrap         = false -- Display long text in one line
+vim.wo.number       = true      -- Show line number
+vim.wo.cursorline   = true      -- Highlight current line number
+vim.wo.wrap         = false     -- Display long text in one line
 vim.wo.signcolumn   = "yes"
 
 -- set up themes
