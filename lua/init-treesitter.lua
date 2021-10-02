7f92fb35bcfafbc73b535e82e4e0413e9579b283
dragove <dragove@qq.com>
2021-10-02 22:15:19 +0800

    Fix incorrect indent

--- lua/init-treesitter.lua
+++ lua/init-treesitter.lua
@@ -1,6 +1,5 @@
 require("nvim-treesitter.configs").setup {
   ensure_installed = "maintained",
-  indent = { enable = true },
   -- auto tag with nvim-ts-autotag
   autotag = { enable = true },
   highlight = { enable = true }
