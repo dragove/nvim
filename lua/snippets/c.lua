c6acc30cf2620b8cacbb718651638c3a2d8929b1
dragove <dragove@qq.com>
2021-11-04 00:40:09 +0800

    Add c printf snippet

--- lua/snippets/c.lua
+++ lua/snippets/c.lua
@@ -18,6 +18,33 @@ local fmta = require('luasnip.extras.fmt').fmta
 local types = require('luasnip.util.types')
 local conds = require('luasnip.extras.expand_conditions')
 
+local function printf_expand(args, _, old_state)
+	local nodes = {}
+  if not old_state then old_state = {} end
+
+  -- count placeholders in string
+  local _, count = args[1][1]:gsub('%%', '')
+  if count == 0 then
+    return sn(nil, nodes)
+  end
+
+  for j = 1, count do
+    local iNode
+    if old_state and old_state[j] then
+      iNode = i(j, old_state[j].old_text)
+    else
+      iNode = i(j, 'arg')
+    end
+    nodes[j * 2 - 1] = t(', ')
+    nodes[j * 2] = iNode
+    old_state[j] = iNode
+  end
+
+  local snip = sn(nil, nodes)
+  snip.old_state = old_state
+  return snip
+end
+
 return {
   -- start template
   s('libsst', {
@@ -28,6 +55,14 @@ return {
         '\t' }),
     i(0),
     t({ '', '}' })
+  }),
+  -- printf with auto-expand args
+  s('printf', {
+    t('printf("'),
+    i(1),
+    t('"'),
+    d(2, printf_expand, 1),
+    t(')')
   })
 }
 
