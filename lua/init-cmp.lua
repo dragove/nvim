9b7f10aa846237e4cc4622da81cc0209bd057d4e
dragove <dragove@qq.com>
2021-11-20 23:50:24 +0800

    adapt cmp sort

--- lua/init-cmp.lua
+++ lua/init-cmp.lua
@@ -38,8 +38,8 @@ cmp.setup({
     end, { 'i', 's' })
   },
   sources = {
-    { name = 'nvim_lsp' },
     { name = 'luasnip' },
+    { name = 'nvim_lsp' },
     { name = 'buffer' },
     { name = 'path' }
   }
