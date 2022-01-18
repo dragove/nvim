15385594fd236e139d07a52e69493f5db371cb9b
jingchun <jingchun@tsign.cn>
2022-01-18 18:18:23 +0800

    add python formmater

--- lua/init-null-ls.lua
+++ lua/init-null-ls.lua
@@ -1,5 +1,7 @@
-require("null-ls").setup({
+local null_ls = require("null-ls")
+null_ls.setup({
     sources = {
-        require("null-ls").builtins.formatting.stylua,
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.black,
     },
 })
