27359419476ef49d919d87f9508e4051a8d32dcc
dragove <dragove@qq.com>
2022-02-03 20:57:05 +0800

    simplify configs

--- lua/init-lspconf.lua
+++ lua/init-lspconf.lua
@@ -1,13 +1,9 @@
 local nvim_lsp = require("lspconfig")
+require("lspsaga").setup()
 local on_attach = function(client, bufnr)
     local function buf_set_keymap(...)
         vim.api.nvim_buf_set_keymap(bufnr, ...)
     end
-    local function buf_set_option(...)
-        vim.api.nvim_buf_set_option(bufnr, ...)
-    end
-
-    buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
 
     -- Mappings.
     local opts = { noremap = true, silent = true }
--- lua/init-telescope.lua
+++ lua/init-telescope.lua
@@ -1,4 +1,5 @@
-require("telescope").setup({
+local telescope = require("telescope")
+telescope.setup({
     defaults = {
         borderchars = { "═", "║", "═", "║", "╔", "╗", "╝", "╚" },
     },
@@ -9,21 +10,12 @@ require("telescope").setup({
         },
     },
 })
-
-require("telescope").load_extension("media_files")
+telescope.load_extension("media_files")
 
 local opt = { noremap = true, silent = true }
-vim.g.mapleader = " "
 -- mappings
 vim.api.nvim_set_keymap("n", "<Leader>sf", [[<Cmd>lua require('telescope.builtin').find_files()<CR>]], opt)
 vim.api.nvim_set_keymap("n", "<Leader>sg", [[<Cmd>lua require('telescope.builtin').live_grep()<CR>]], opt)
 vim.api.nvim_set_keymap("n", "<Leader>sb", [[<Cmd>lua require('telescope.builtin').buffers()<CR>]], opt)
 vim.api.nvim_set_keymap("n", "<Leader>sh", [[<Cmd>lua require('telescope.builtin').help_tags()<CR>]], opt)
-
--- highlights
-local cmd = vim.cmd
-
-cmd("hi TelescopeBorder   guifg=#2a2e36")
-cmd("hi TelescopePromptBorder   guifg=#2a2e36")
-cmd("hi TelescopeResultsBorder  guifg=#2a2e36")
-cmd("hi TelescopePreviewBorder  guifg=#525865")
+vim.api.nvim_set_keymap("n", "<Leader>sp", [[<Cmd>lua require('telescope').extensions.media_files.media_files()<CR>]], opt)
