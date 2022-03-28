b189374755829881c25269e17bbcca86eceede89
dragove <dragove@qq.com>
2022-03-28 19:44:56 +0800

    change nvim-tree keybinding and update config

--- lua/init-tree.lua
+++ lua/init-tree.lua
@@ -1,18 +1,11 @@
-vim.g.nvim_tree_quit_on_open = 1
 require("nvim-tree").setup({
     update_focused_file = {
         enable = true,
-    },
-    view = {
-        auto_resize = true,
-    },
-    filters = {
-        custom = { ".git", "node_modules" },
-    },
+    }
 })
 
 -- Mappings for nvimtree
-vim.api.nvim_set_keymap("n", "<C-e>", ":NvimTreeToggle<CR>", {
+vim.api.nvim_set_keymap("n", "<C-n>", ":NvimTreeToggle<CR>", {
     noremap = true,
     silent = true,
 })
