dfdbe3c99e8165556e18c47040d400b5368751a5
dragove <dragove@qq.com>
2022-04-17 15:32:45 +0800

    Adapt luasnip snippets to fix breaking changes in luasnip

--- lua/snippets/init.lua
+++ lua/snippets/init.lua
@@ -1,6 +1,6 @@
-require("luasnip").snippets = {
+require("luasnip").add_snippets(nil, {
     c = require("snippets/c"),
     javascript = require("snippets/javascript"),
     javascriptreact = require("snippets/javascriptreact"),
     python = require("snippets/python"),
-}
+})
