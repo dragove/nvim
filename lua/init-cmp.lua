eb467f3033c0249f02d4a2ef9f7ddd06a00a0a16
dragove <dragove@qq.com>
2022-10-12 22:44:30 +0800

    add cmp for search and cmdline

--- lua/init-cmp.lua
+++ lua/init-cmp.lua
@@ -51,6 +51,24 @@ cmp.setup({
     }),
 })
 
+-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline({ "/", "?" }, {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+        { name = "buffer" }
+    }
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(":", {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+        { name = "path" }
+    }, {
+        { name = "cmdline" }
+    })
+})
+
 require("nvim-autopairs").setup()
 
 local cmp_autopairs = require("nvim-autopairs.completion.cmp")
--- lua/init-plugins.lua
+++ lua/init-plugins.lua
@@ -49,6 +49,7 @@ return require("packer").startup(function(use)
         requires = {
             { "hrsh7th/cmp-path" },
             { "hrsh7th/cmp-buffer" },
+            { "hrsh7th/cmp-cmdline" },
             { "hrsh7th/cmp-nvim-lsp" },
             -- snippet support
             { "L3MON4D3/LuaSnip" },
