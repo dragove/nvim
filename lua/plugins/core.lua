935e8c0938ad267649ef43d88d95f5ab43cc95bc
dragove <dragove@qq.com>
2023-02-18 15:55:45 +0800

    disable nvim-notify

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -1,15 +1,16 @@
 return {
-	{
-		"LazyVim/LazyVim",
-		opts = {
-			colorscheme = "onedark",
-		},
-		dependencies = {
-			"olimorris/onedarkpro.nvim",
-		},
-	},
-	-- disable some plugins
-	{ "folke/tokyonight.nvim", enabled = false },
-	{ "catppuccin", enabled = false },
-	{ "vim-startuptime", enabled = false },
+    {
+        "LazyVim/LazyVim",
+        opts = {
+            colorscheme = "onedark",
+        },
+        dependencies = {
+            "olimorris/onedarkpro.nvim",
+        },
+    },
+    -- disable some plugins
+    { "folke/tokyonight.nvim", enabled = false },
+    { "catppuccin",            enabled = false },
+    { "vim-startuptime",       enabled = false },
+    { "rcarriga/nvim-notify",  enabled = false },
 }
